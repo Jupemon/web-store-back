29cb1a6cdf8204c2e70538c88f29ea6d87ed158b
Juho <s6heju04@gmail.com>
2019-09-05 10:26:58 +0300

    Added database connections

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
+
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+          
+        /*
         host: '127.0.0.1',
         user : 'postgres',
         password : 'test',
         database: 'postgres'
+        */
     }
 });
 
