c26186c692feb3061dbe6ca3155e3c84170e3af2
Juho <s6heju04@gmail.com>
2019-09-12 13:22:11 +0300

    added client to knex configuration

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const db = process.env.DATABASE_URL ? knex({
+    client: 'pg',
     connectionString: process.env.DATABASE_URL,
     ssl: true
 }) : knex({
