a5a70570fa5a445035e9ef58f0103f83dce179d1
Juho <s6heju04@gmail.com>
2019-11-13 16:46:45 +0200

    another try to fix the image serving

--- Controllers/image.js
+++ Controllers/image.js
@@ -7,7 +7,7 @@ const serveImage = (req, res, db) => {
     db('products').where('productid', id).then(data => {
         console.log(data[0].productimage, "PRODUCT IMAGE DATA HERE")
         console.log(data[0].productimage.replace("uploads", ""), "PRODUCT DATA CHANGED HERE")
-        res.sendFile(data[0].productimage.replace("/uploads", ""), (err) => {
+        res.sendFile(data[0].productimage.replace("/uploads", ""), options, (err) => {
             if (err) {
                 res.status(404);
                 res.json(err)
